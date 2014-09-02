6e0039b52bc22d5e37a25778c0905f88ad352231
Edwin Wiersma <eddie1707@live.nl>
2014-09-02 14:19:26 +0200

    Initial Commit

---
+++ Overdue/EWViewController.h
@@ -0,0 +1,13 @@
+//
+//  EWViewController.h
+//  Overdue
+//
+//  Created by Edwin Wiersma on 02/09/14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EWViewController : UIViewController
+
+@end
---
+++ Overdue/EWViewController.m
@@ -0,0 +1,29 @@
+//
+//  EWViewController.m
+//  Overdue
+//
+//  Created by Edwin Wiersma on 02/09/14.
+//  Copyright (c) 2014 Apps4Mobile. All rights reserved.
+//
+
+#import "EWViewController.h"
+
+@interface EWViewController ()
+
+@end
+
+@implementation EWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
